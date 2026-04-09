37a7fcaef477a61a6ae6332bf9eb95d91b8d1aea
tebyappsandgames-droid <tebyappsandgames@gmail.com>
2026-04-09 14:39:02 -0300

    Update bathtime_screen.dart

--- lib/screens/bathtime_screen.dart
+++ lib/screens/bathtime_screen.dart
@@ -1,51 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:animated_widgets/animated_widgets.dart';
 
-class BathtimeScreen extends StatefulWidget {
-  @override
-  _BathtimeScreenState createState() => _BathtimeScreenState();
-}
-
-class _BathtimeScreenState extends State<BathtimeScreen> {
-  bool isSoapApplied = false;
+class BathtimeScreen extends StatelessWidget {
+  const BathtimeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/backgrounds/bathroom_background.png'),
             fit: BoxFit.cover,
           ),
         ),
-        child: Stack(
-          children: [
-            Center(
-              child: Image.asset('assets/characters/hope_ferrer.png', height: 300),
-            ),
-            Positioned(
-              right: 30,
-              top: 50,
-              child: Draggable(
-                data: 'soap',
-                feedback: Image.asset('assets/items_and_objects/bar_of_soap.png', width: 100),
-                child: Image.asset('assets/items_and_objects/bar_of_soap.png', width: 80),
+        child: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Image.asset('assets/characters/hope_ferrer.png', height: 350),
+              Positioned(
+                bottom: 20,
+                child: Row(
+                  children: [
+                    Image.asset('assets/items_and_objects/bar_of_soap.png', width: 60),
+                    Image.asset('assets/items_and_objects/baby_shampoo.png', width: 60),
+                    Image.asset('assets/items_and_objects/sponge.png', width: 60),
+                    Image.asset('assets/items_and_objects/rubber_duck.png', width: 60),
+                  ],
+                ),
               ),
-            ),
-            DragTarget<String>(
-              onAccept: (data) {
-                if (data == 'soap') setState(() => isSoapApplied = true);
-              },
-              builder: (context, candidate, rejected) {
-                return Center(
-                  child: isSoapApplied 
-                    ? Opacity(opacity: 0.5, child: Image.asset('assets/items_and_objects/sponge.png', width: 200))
-                    : Container(width: 300, height: 300),
-                );
-              },
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
